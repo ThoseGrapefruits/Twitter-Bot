c688ffbbdb2a728eb8287281131680a8fbab1066
Logan Moore <lmooore@gmail.com>
2015-10-07 10:32:34 -0400

    Created Constants

---
+++ src/me/loganmoore/constants/Constants.java
@@ -0,0 +1,8 @@
+package me.loganmoore.constants;
+
+import java.nio.charset.Charset;
+
+public class Constants {
+
+  public static final Charset CHARSET = java.nio.charset.StandardCharsets.UTF_8;
+}
